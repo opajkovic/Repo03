45e1f10cb51ed6e7e3753f1519a3cebe050dedba
opajkovic <oljaolja170@gmail.com>
2022-03-30 15:18:48 +0200

    added ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo) {
+    var product;
+    product = numOne * numTwo
+
+    return product;
+}
+
+console.log(main(15, 3));
+console.log(main(25, 8));
+console.log(main(425, 10));
+
+module.export = main;
