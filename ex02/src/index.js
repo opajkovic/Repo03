2c4cd8e9287740fe890ee74735ff4c92e70efb4a
opajkovic <oljaolja170@gmail.com>
2022-03-30 13:19:18 +0200

    added ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // This is a single line JavaScript comment//
+    /* This is a multi line comment!
+    console.log('Hello World!');*/
+}
+
+console.log(main());
+
+module.export - main;
