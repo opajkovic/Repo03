26ca034fd8dac26e475679d2c69cc41f7e835f44
opajkovic <oljaolja170@gmail.com>
2022-03-30 14:50:57 +0200

    added ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+
+    var studlyCapVar = 10;
+    var properCamelCase = "A String";
+    var titleCaseOver = 9000;
+
+    return {
+        studlyCapVar,
+        properCamelCase,
+        titleCaseOver
+    }
+}
+
+console.log(main());
+
+module.export = main;
