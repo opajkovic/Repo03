e70d4d27d271e7d87fa72f617f2cd4a46c2230aa
opajkovic <oljaolja170@gmail.com>
2022-03-30 14:58:57 +0200

    added ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo) {
+    var sum;
+    sum = numOne + numTwo;
+
+    return sum;
+}
+
+console.log(main(15, 8));
+console.log(main(5, 28));
+console.log(main(150, 425));
+
+module.export = main;
